051eab10eceddb592fbff1f70a7f5bac48a555e6
omegadc <dcleader99@gmail.com>
2024-04-06 20:44:33 -0400

    Configured to have all models appear on flask.

--- backend/models.py
+++ backend/models.py
@@ -14,41 +14,42 @@ class Student(db.Model):
 
 
 class Event(db.Model):
-    __tablename__ = 'event'
+    __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
-    eventID = db.Column(db.Integer, primary_key=True, nullable=False)
+    eventID = db.Column(db.Integer, nullable=False)
     Name = db.Column(db.Text, nullable=False)
     # Date = db.Column(db.Datetime, nullable=False)
     Location = db.Column(db.Text,nullable=False)
     Description = db.Column(db.Text)
 
     def __repr__(self):
-        return f"User full name is {self.firstName} {self.lastName} and their email is {self.email}, their age is {self.age}"
+        return f"Event{self.eventID}: {self.Name} and the location {self.Location} with a Description: {self.Description}."
 
 
 class RSVP(db.Model):
     __tablename__ = 'rsvp'
     id = db.Column(db.Integer, primary_key=True)
     studentID = db.Column(db.Integer, nullable=False)
-    eventID = db.Column(db.Integer, primary_key=True, nullable=False)
+    eventID = db.Column(db.Integer, nullable=False)
     rsvpStatus = db.Column(db.Text, nullable=False)
     # rsvpDate = db.Column(db.Datetime, nullable=False)
 
     def __repr__(self):
-        return f"User full name is {self.firstName} {self.lastName} and their email is {self.email}, their age is {self.age}"
+        return f"{self.studentID} send a status {self.rsvpStatus} for {self.eventID}."
 
 
 class Attendance(db.Model):
+    __tablename__ = 'attendance'
     id=db.Column(db.Integer, primary_key=True, nullable=False)
-    studentID=db.Column(db.Integer, foreign_key=True, nullable=False)
-    eventID=db.Column(db.Integer, foreign_key=True, nullable=False)
+    studentID=db.Column(db.Integer, nullable=False)
+    eventID=db.Column(db.Integer, nullable=False)
     status=db.Column(db.Boolean)
     # date=db.Column(db.Datetime,nullable=False)
     def __repr__(self):
-        return f"User full name is {self.firstName} {self.lastName} and their email is {self.email}, their age is {self.age}"
-
+        return f"{self.studentID} is in {self.status} for {self.eventID}"
 
 class Advisor(db.Model):
+    __tablename__='advisors'
     id=db.Column(db.Integer, primary_key=True, nullable=False)
     firstName = db.Column(db.Text, nullable=False)
     lastName = db.Column(db.Text, nullable=False)
@@ -56,14 +57,15 @@ class Advisor(db.Model):
     phoneNumber = db.Column(db.Text)
     department = db.Column(db.Text)
     def __repr__(self):
-        return f"User full name is {self.firstName} {self.lastName} and their email is {self.email}, their age is {self.age}"
+        return f"Advisor {self.id}: Full name: {self.firstName} {self.lastName}, Email: {self.email}, Department: {self.department}, PhoneNum: {self.phoneNumber}"
 
 
 class AppointmentID(db.Model):
+    __tablename__='appointments'
     id=db.Column(db.Integer, primary_key=True, nullable=False)
-    studentID=db.Column(db.Integer, foreign_key=True, nullable=False)
-    AdvisorID=db.Column(db.Integer, foreign_key=True, nullable=False)
+    studentID=db.Column(db.Integer, nullable=False)
+    AdvisorID=db.Column(db.Integer, nullable=False)
     # date=db.Column(db.Datetime, nullable=False)
     status=db.Column(db.Text, nullable=False)
     def __repr__(self):
-        return f"User full name is {self.firstName} {self.lastName} and their email is {self.email}, their age is {self.age}"
+        return f"AppointmentID:{self.id} for StudentID:{self.studentID} and Advisor:{self.AdvisorID} with status:{self.status}"
